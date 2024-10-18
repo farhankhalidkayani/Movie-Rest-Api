1437fcc201acf2838cb827aabfd359b78702682c
Farhan Khalid <rj.farhan4232@gmail.com>
2024-10-18 19:53:45 +0500

    Created Serializer made viewpoints and mapped them to url with router

--- movsite/movies/views.py
+++ movsite/movies/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from .serializers import MovieSerializer
+from .models import MovieData
+from rest_framework import viewsets
+
 
 # Create your views here.
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = MovieData.objects.all()
+    serializer_class = MovieSerializer
